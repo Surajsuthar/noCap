3f9d3ee709c156ef6df1edaf74e612cd3e92b1bb
Suraj Suthar <surajsuthar0067@gmail.com>
2026-04-24 16:45:22 +0530

    added polar docs

--- server/core/config.py
+++ server/core/config.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
 	GOOGLE_CLIENT_ID: str = ""
 	GOOGLE_CLIENT_SECRET: str = ""
 	GOOGLE_REDIRECT_URI: str = "http://localhost:8000/auth/google/callback"
+	POLAR_ACCESS_TOKEN: str = ""
 
 	class Config:
 		env_file = ".env"
